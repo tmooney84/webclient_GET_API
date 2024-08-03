a067d2f9fb92b806dd512e26b99939d102bab78f
Trevor <trevordmooney@gmail.com>
2024-08-03 13:39:42 -0700

    -update as of 1:40pm pst 8-3-24

---
+++ main/java/org/example/getapi/model/LowerTrestles.java
@@ -0,0 +1,11 @@
+package org.example.getapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LowerTrestles {
+    @JsonProperty("timestamp");
+    Long timestamp;
+}
---
+++ test/java/org/example/getapi/GetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.getapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
