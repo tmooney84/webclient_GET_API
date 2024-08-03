a6ee4def05ac3697e05b6cebe3d04beec0fb919a
Trevor <trevordmooney@gmail.com>
2024-08-03 13:43:12 -0700

    -update as of 1:40pm pst 8-3-24

--- main/java/org/example/getapi/model/LowerTrestles.java
+++ main/java/org/example/getapi/model/LowerTrestles.java
@@ -2,7 +2,12 @@ package org.example.getapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LowerTrestles {
